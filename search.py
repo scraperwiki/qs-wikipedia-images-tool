4424c42607e2a53667b16a7ca824a5b100337276
ehg <chris.blower@jumpwire.co.uk>
2013-05-20 15:53:14 +0100

    use requests to parse json

--- search.py
+++ search.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 import sys
-import json
 import requests
 import scraperwiki
 
-response = requests.get("http://en.wikipedia.org/w/api.php?action=query&list=allimages&aiprop=url|user|timestamp|size|mime&ailimit=100&format=json&aifrom=%s" % sys.argv[1])
+URL = "http://en.wikipedia.org/w/api.php?action=query&list=allimages&aiprop=url|user|timestamp|size|mime&ailimit=100&format=json&aifrom=%s"
 
-data = json.loads(response.text)
-
+data = requests.get(URL % sys.argv[1]).json()
 images = data['query']['allimages']
 scraperwiki.sqlite.save(['name'], images, 'images')
